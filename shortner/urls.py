5d21a23db134c896e4797464edf5313ab5ca0db1
isayaeli <isayaelib@gmail.com>
2022-06-27 20:46:00 +0300

    Changes for deployment

--- shortner/urls.py
+++ shortner/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('success', views.success, name='success'),
      path('edit/<str:slug>', views.update_url, name='edit'),
     path('disable/<str:slug>', views.disable_url,name='disable'),
-    path('delete/<str:slug>', views.delete_url, name='delete'),
+    path('delete/<str:id>', views.delete_url, name='delete'),
     # path('<str:slug>', views.exit, name='exit'),
     
 ]
